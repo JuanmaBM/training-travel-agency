34896ba12d41e0dc388b9783de13159b986b0e58
Juan Manuel Barea Martinez <jmbarea@atsistemas.com>
2020-03-21 11:46:07 +0100

    Adds HUB Agency project

---
+++ travel-hub-agency/src/main/kotlin/org/jmb/travelhubagency/TravelHubAgencyApplication.kt
@@ -0,0 +1,11 @@
+package org.jmb.travelhubagency
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelHubAgencyApplication
+
+fun main(args: Array<String>) {
+	runApplication<TravelHubAgencyApplication>(*args)
+}
